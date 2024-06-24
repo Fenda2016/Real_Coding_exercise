13bea8a37ac27d3687078049d682f36dac17a781
Fengda <fengdaliu@atal.com>
2024-06-24 11:20:56 +0800

    first commit test

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import csv
-import sqlite3
 import os
 import psycopg2
+from datetime import datetime
 
-"""we have a DB table for three colums"""
-"""read the CSV files and insert the data into the Database"""
-# Connect to the database
+# Connect to the database 
 conn = psycopg2.connect(
-    host="your_host",
-    database="your_database",
-    user="your_user",
-    password="your_password"
+    port=int(os.environ.get("DB_PORT", 5432)),
+    host=os.environ.get("DB_HOST"),
+    database=os.environ.get("DB_NAME"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
 )
-
+directory = os.environ.get("DIRECTORY")
 cursor = conn.cursor()
-with open('data.csv', 'r') as file:
-    reader = csv.reader(file)
-    next(reader)  # Skip the header row
-    for row in reader:
-        # Insert the data into the database
-        cursor.execute('INSERT INTO table_name (column1, column2, column3) VALUES (?, ?, ?)', row)
+"""OPEN THE FILES UNDER THE DIRECTORY"""
+import concurrent.futures
 
+# Get the list of files in the directory
+files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith('.csv')]
+
+# Function to process a CSV file
+def process_csv(file):
+    file_path = os.path.join(directory, file)
+    with open(file_path, 'r') as file:
+        try:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            for row in reader:
+                try:
+                    # Check if row[0] is in ISO 8601 format
+                    datetime.fromisoformat(row[0])
+                    # Check if row[2] is numeric
+                    if not row[2].isnumeric(): 
+                        raise ValueError(f"Non-numeric value found: {row[2]}")
+                    # Insert the data into the database
+                    cursor.execute('INSERT INTO newtable (timestamp, sensorname, value) VALUES (%s, %s, %s) ON CONFLICT(timestamp,sensorname) DO UPDATE SET value=excluded.value', (row[0], row[1], row[2]))
+                except ValueError as ve:
+                    print("Value Error occured: ", ve)
+        except Exception as e:
+            print(f"Error reading CSV file: {e}")
+
+# Process the CSV files in parallel
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(process_csv, files)
 # Commit the changes and close the connection
 conn.commit()
 conn.close()
